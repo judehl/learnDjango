54001ee104a7e4d346d2ac1b3475296b4fa4f36b
judehl <1978572916@qq.com>
2022-02-03 21:11:33 +0800

    update

--- README.md
+++
@@ -1,2 +0,0 @@
-# learnDjango
-django学习
--- test_platform/api_app/user_view.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import User
-from rest_framework import viewsets
-from api_app.serializer import UserSerializer
-
-
-# ViewSets define the view behavior.
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
