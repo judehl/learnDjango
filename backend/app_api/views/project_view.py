7fc405a39d16a9fc4ca48cc5b179bdc5143e8ac5
judehl <1978572916@qq.com>
2022-02-22 21:49:12 +0800

    feat:update learn rest_framework

--- backend/app_api/views/project_view.py
+++ backend/app_api/views/project_view.py
@@ -1,29 +1,38 @@
 from django.shortcuts import render
-from rest_framework.views import APIView
 from django.http import JsonResponse
 from app_api.serializer.project import ProjectValidator, ProjectSerializer
 from app_api.models.project_model import Project
 from app_common.utils.pagination import Pagination
 from app_common.utils.response import response, Error
+from app_common.utils.base_view import BaseAPIView
+
 
 # Create your views here.
-class ProjectView(APIView):
+class ProjectView(BaseAPIView):
 
     def get(self, request, *args, **kwargs):
         pid = kwargs.get('pk')
+        page = request.query_params.get('page', 1)  # 分页查询获取页码数
+        size = request.query_params.get('size', 5)  # 分页查询获取每页条数
         if pid is not None:  # 查询单条
             try:
                 project = Project.objects.get(pk=pid, is_delete=False)
                 ser = ProjectSerializer(instance=project, many=False)
             except Project.DoesNotExist:
-                return JsonResponse({'msg': 'project object is null'})
-            return JsonResponse({'msg': '查询成功', 'data': ser.data})
+                return self.response_fail(error=self.PROJECT_ID_NULL)
+            return self.response(data=ser.data)
         else:  # 查询多条数据
             project = Project.objects.filter(is_delete=False).all()
             pg = Pagination()
             page_data = pg.paginate_queryset(queryset=project, request=request, view=self)
             ser = ProjectSerializer(instance=page_data, many=True)
-            return JsonResponse({'msg': '查询成功', 'data': ser.data})
+            data = {
+                'total': len(project),
+                'page': int(page),  # 分页信息不输默认第一页，每页5条
+                'size': int(size),
+                'projectList': ser.data
+            }
+            return self.response(data=data)
 
     def post(self, request, *args, **kwargs):
         """
@@ -33,30 +42,30 @@ class ProjectView(APIView):
         if val.is_valid():  # 字段验证通过
             val.save()
         else:
-            return JsonResponse({'msg': '添加', 'error': val.errors})
-        return JsonResponse({'msg': '添加成功'})
+            return self.response_fail(error=val.errors)
+        return self.response()
 
     def put(self, request, *args, **kwargs):
         """
-        id写在url中 更新：使用kwargs，id通过写在请求参数中更新使用request.data.get
+        id写在url中更新：使用kwargs
+        id通过请求参数更新使用request.data.get
         """
         pid = kwargs.get('pk')  # url 中 获取
         # pid = request.data.get('id')  # 请求参数中获取
         if pid is None:
-            return JsonResponse({'msg': "project id is null"})
+            return self.response(error=self.PROJECT_ID_NULL)
         try:
             project = Project.objects.get(pk=pid, is_delete=False)
         except Project.DoesNotExist:
-            return JsonResponse({"msg": "Project object is null"})
+            return self.response(error=self.PROJECT_OBJECT_NULL)
 
         # 更新
         val = ProjectValidator(instance=project, data=request.data)
         if val.is_valid():
             val.save()
         else:
-            return JsonResponse({'msg': '更新失败', 'error': val.errors})
-
-        return JsonResponse({'msg': '更新'})
+            return self.response(error=val.errors)
+        return self.response()
 
     def delete(self, request, *args, **kwargs):
         """
@@ -67,5 +76,5 @@ class ProjectView(APIView):
             project = Project.objects.filter(pk=pid, is_delete=False).update(is_delete=True)
             print(project)
             if project == 0:
-                return JsonResponse({'msg': '删除失败'})
-            return JsonResponse({'msg': '删除成功'})
+                return self.response(error=self.PROJECT_DELETE_ERROR)
+            return self.response()
